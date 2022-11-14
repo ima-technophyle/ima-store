c90fc31d106677be58b3f48d45f9a07e2775a60d
ima-technophyle <siva@Sivasankers-MacBook-Pro.local>
2022-11-14 13:45:18 +0530

    Fixed env variables

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express")();
-const { has, get } = require("config");
+const config = require("config");
 const logger = require("./logger/logging");
 require("./error handler/errorHandler");
 require("./mongoose/config");
 require("./routes/route")(express);
 
-const port = has("port") ? get("port") : 3009;
+const port = config.get("port");
 const server = express.listen(port, () => {
   logger.info(`App started at port: ${port}`);
 });
